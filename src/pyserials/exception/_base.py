8651b971a7860b504373a30db012e79c14403c1c
RepoDynamicsBot <80158628+AAriam@users.noreply.github.com>
2024-09-26 22:28:59 +0200

    Release version 0.0.0.dev17

--- src/pyserials/exception/_base.py
+++ src/pyserials/exception/_base.py
@@ -3,8 +3,10 @@
 from __future__ import annotations
 
 from typing import TYPE_CHECKING as _TYPE_CHECKING
+from functools import partial as _partial
 
 from exceptionman import ReporterException as _ReporterException
+import mdit as _mdit
 
 if _TYPE_CHECKING:
     from mdit import Document
@@ -14,13 +16,13 @@ class PySerialsException(_ReporterException):
     """Base class for all exceptions raised by PySerials."""
 
     def __init__(self, report: Document):
-        super().__init__(
-            report=report,
-            sphinx_config={
-                "extensions": ['myst_parser', 'sphinx_togglebutton'],
-                "myst_enable_extensions": ["colon_fence", "fieldlist"],
-                "html_theme": "pydata_sphinx_theme",
-                "html_title": "PySerials Error Report",
-            }
+        sphinx_config = {"html_title": "PySerials Error Report"}
+        sphinx_target_config = _mdit.target.sphinx(
+            renderer=_partial(
+                _mdit.render.sphinx,
+                config=_mdit.render.get_sphinx_config(sphinx_config)
+            )
         )
+        report.target_configs["sphinx"] = sphinx_target_config
+        super().__init__(report=report)
         return
